bd7d6f0977931387c09f0a0b5a2a58e50a77c63e
Charles Pick <charles.pick@gmail.com>
2013-03-31 20:17:41 +0100

    Added tag v2.3.1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function(){
   var jQuery = require('jquery');
   /* ========================================================
-   * bootstrap-tab.js v2.3.0
+   * bootstrap-tab.js v2.3.1
    * http://twitter.github.com/bootstrap/javascript.html#tabs
    * ========================================================
    * Copyright 2012 Twitter, Inc.
